6aac807eee0a2abb848e8b5876a26308384bd0de
0x1 <kevin.francart3@gmail.com>
2026-08-01 13:50:00 +0200

    fix(ffi): fix various FFI implementations

---
+++ src/Node/FS/Async.go
@@ -0,0 +1,69 @@
+package Node_FS_Async
+
+import (
+    "os"
+    "fmt"
+)
+
+func unboxString(v interface{}) string {
+    return v.(string)
+}
+
+func MkdirImpl(path string, opts interface{}, cb func(interface{}, interface{}) interface{}) interface{} {
+    err := os.MkdirAll(path, 0755)
+    if err != nil {
+        cb(err, nil)
+    } else {
+        cb(nil, nil)
+    }
+    return nil
+}
+
+func ReadFileImpl(filepath string, opts interface{}, cb func(interface{}, interface{}) interface{}) interface{} {
+    fmt.Printf("DEBUG: ReadFileImpl called with %s\n", filepath)
+    data, err := os.ReadFile(filepath)
+    if err != nil {
+        fmt.Printf("DEBUG: ReadFileImpl error calling cb\n")
+        cb(err, nil)
+        fmt.Printf("DEBUG: ReadFileImpl error cb returned\n")
+    } else {
+        cb(nil, string(data)) // assuming encoding is utf8 for now
+    }
+    return nil
+}
+
+func WriteFileImpl(filepath string, content string, opts interface{}, cb func(interface{}, interface{}) interface{}) interface{} {
+    data := []byte(content)
+    err := os.WriteFile(filepath, data, 0644)
+    if err != nil {
+        cb(err, nil)
+    } else {
+        cb(nil, nil)
+    }
+    return nil
+}
+
+func AccessImpl(arg0 interface{}, arg1 interface{}, arg2 interface{}) interface{} { panic("Not implemented: accessImpl"); return nil }
+func CopyFileImpl(arg0 interface{}, arg1 interface{}, arg2 interface{}, arg3 interface{}) interface{} { panic("Not implemented: copyFileImpl"); return nil }
+func MkdtempImpl(arg0 interface{}, arg1 interface{}, arg2 interface{}) interface{} { panic("Not implemented: mkdtempImpl"); return nil }
+func RenameImpl(arg0 interface{}, arg1 interface{}, arg2 interface{}) interface{} { panic("Not implemented: renameImpl"); return nil }
+func TruncateImpl(arg0 interface{}, arg1 interface{}, arg2 interface{}) interface{} { panic("Not implemented: truncateImpl"); return nil }
+func ChownImpl(arg0 interface{}, arg1 interface{}, arg2 interface{}, arg3 interface{}) interface{} { panic("Not implemented: chownImpl"); return nil }
+func ChmodImpl(arg0 interface{}, arg1 interface{}, arg2 interface{}) interface{} { panic("Not implemented: chmodImpl"); return nil }
+func StatImpl(arg0 interface{}, arg1 interface{}) interface{} { panic("Not implemented: statImpl"); return nil }
+func LstatImpl(arg0 interface{}, arg1 interface{}) interface{} { panic("Not implemented: lstatImpl"); return nil }
+func LinkImpl(arg0 interface{}, arg1 interface{}, arg2 interface{}) interface{} { panic("Not implemented: linkImpl"); return nil }
+func SymlinkImpl(arg0 interface{}, arg1 interface{}, arg2 interface{}, arg3 interface{}) interface{} { panic("Not implemented: symlinkImpl"); return nil }
+func ReadlinkImpl(arg0 interface{}, arg1 interface{}) interface{} { panic("Not implemented: readlinkImpl"); return nil }
+func RealpathImpl(arg0 interface{}, arg1 interface{}, arg2 interface{}) interface{} { panic("Not implemented: realpathImpl"); return nil }
+func UnlinkImpl(arg0 interface{}, arg1 interface{}) interface{} { panic("Not implemented: unlinkImpl"); return nil }
+func RmdirImpl(arg0 interface{}, arg1 interface{}, arg2 interface{}) interface{} { panic("Not implemented: rmdirImpl"); return nil }
+func RmImpl(arg0 interface{}, arg1 interface{}, arg2 interface{}) interface{} { panic("Not implemented: rmImpl"); return nil }
+func ReaddirImpl(arg0 interface{}, arg1 interface{}) interface{} { panic("Not implemented: readdirImpl"); return nil }
+func UtimesImpl(arg0 interface{}, arg1 interface{}, arg2 interface{}, arg3 interface{}) interface{} { panic("Not implemented: utimesImpl"); return nil }
+func AppendFileImpl(arg0 interface{}, arg1 interface{}, arg2 interface{}, arg3 interface{}) interface{} { panic("Not implemented: appendFileImpl"); return nil }
+func OpenImpl(arg0 interface{}, arg1 interface{}, arg2 interface{}, arg3 interface{}) interface{} { panic("Not implemented: openImpl"); return nil }
+func ReadImpl(arg0 interface{}, arg1 interface{}, arg2 interface{}, arg3 interface{}, arg4 interface{}, arg5 interface{}) interface{} { panic("Not implemented: readImpl"); return nil }
+func WriteImpl(arg0 interface{}, arg1 interface{}, arg2 interface{}, arg3 interface{}, arg4 interface{}, arg5 interface{}) interface{} { panic("Not implemented: writeImpl"); return nil }
+func CloseImpl(arg0 interface{}, arg1 interface{}) interface{} { panic("Not implemented: closeImpl"); return nil }
+
---
+++ src/Node/FS/Sync.go
@@ -0,0 +1,158 @@
+package Node_FS_Sync
+
+import (
+    "os"
+)
+
+func ReadFileImpl(filepath string, opts interface{}) interface{} {
+    data, err := os.ReadFile(filepath)
+    if err != nil {
+        panic(err)
+    }
+    return string(data)
+}
+
+func WriteFileImpl(filepath string, content string, opts interface{}) interface{} {
+    data := []byte(content)
+    err := os.WriteFile(filepath, data, 0644)
+    if err != nil {
+        panic(err)
+    }
+    return nil
+}
+
+func AccessImpl(arg0 interface{}, arg1 interface{}) interface{} {
+    panic("Not implemented: accessImpl")
+    return nil
+}
+
+func CopyFileImpl(arg0 interface{}, arg1 interface{}, arg2 interface{}) interface{} {
+    panic("Not implemented: copyFileImpl")
+    return nil
+}
+
+func MkdtempImpl(arg0 interface{}, arg1 interface{}) interface{} {
+    panic("Not implemented: mkdtempImpl")
+    return nil
+}
+
+func RenameSyncImpl(arg0 interface{}, arg1 interface{}) interface{} {
+    panic("Not implemented: renameSyncImpl")
+    return nil
+}
+
+func TruncateSyncImpl(arg0 interface{}, arg1 interface{}) interface{} {
+    panic("Not implemented: truncateSyncImpl")
+    return nil
+}
+
+func ChownSyncImpl(arg0 interface{}, arg1 interface{}, arg2 interface{}) interface{} {
+    panic("Not implemented: chownSyncImpl")
+    return nil
+}
+
+func ChmodSyncImpl(arg0 interface{}, arg1 interface{}) interface{} {
+    panic("Not implemented: chmodSyncImpl")
+    return nil
+}
+
+func StatSyncImpl(arg0 interface{}) interface{} {
+    panic("Not implemented: statSyncImpl")
+    return nil
+}
+
+func LstatSyncImpl(arg0 interface{}) interface{} {
+    panic("Not implemented: lstatSyncImpl")
+    return nil
+}
+
+func LinkSyncImpl(arg0 interface{}, arg1 interface{}) interface{} {
+    panic("Not implemented: linkSyncImpl")
+    return nil
+}
+
+func SymlinkSyncImpl(arg0 interface{}, arg1 interface{}, arg2 interface{}) interface{} {
+    panic("Not implemented: symlinkSyncImpl")
+    return nil
+}
+
+func ReadlinkSyncImpl(arg0 interface{}) interface{} {
+    panic("Not implemented: readlinkSyncImpl")
+    return nil
+}
+
+func RealpathSyncImpl(arg0 interface{}, arg1 interface{}) interface{} {
+    panic("Not implemented: realpathSyncImpl")
+    return nil
+}
+
+func UnlinkSyncImpl(arg0 interface{}) interface{} {
+    panic("Not implemented: unlinkSyncImpl")
+    return nil
+}
+
+func RmdirSyncImpl(path string, opts interface{}) interface{} {
+    err := os.Remove(path) // os.Remove acts as rmdir for directories if empty
+    if err != nil {
+        panic(err)
+    }
+    return nil
+}
+
+func RmSyncImpl(path string, opts interface{}) interface{} {
+    err := os.RemoveAll(path)
+    if err != nil {
+        panic(err)
+    }
+    return nil
+}
+
+func MkdirSyncImpl(path string, opts interface{}) interface{} {
+    err := os.MkdirAll(path, 0755)
+    if err != nil {
+        panic(err)
+    }
+    return nil
+}
+
+func ReaddirSyncImpl(path string) interface{} {
+    entries, err := os.ReadDir(path)
+    if err != nil {
+        panic(err)
+    }
+    names := make([]interface{}, len(entries))
+    for i, entry := range entries {
+        names[i] = entry.Name()
+    }
+    return names
+}
+
+func UtimesSyncImpl(arg0 interface{}, arg1 interface{}, arg2 interface{}) interface{} {
+    panic("Not implemented: utimesSyncImpl")
+    return nil
+}
+
+func AppendFileSyncImpl(arg0 interface{}, arg1 interface{}, arg2 interface{}) interface{} {
+    panic("Not implemented: appendFileSyncImpl")
+    return nil
+}
+
+func OpenSyncImpl(arg0 interface{}, arg1 interface{}, arg2 interface{}) interface{} {
+    panic("Not implemented: openSyncImpl")
+    return nil
+}
+
+func ReadSyncImpl(arg0 interface{}, arg1 interface{}, arg2 interface{}, arg3 interface{}, arg4 interface{}) interface{} {
+    panic("Not implemented: readSyncImpl")
+    return nil
+}
+
+func WriteSyncImpl(arg0 interface{}, arg1 interface{}, arg2 interface{}, arg3 interface{}, arg4 interface{}) interface{} {
+    panic("Not implemented: writeSyncImpl")
+    return nil
+}
+
+func CloseSyncImpl(arg0 interface{}) interface{} {
+    panic("Not implemented: closeSyncImpl")
+    return nil
+}
