95a7e40bbdfa5e50bc275d5ee9a1a65836b269ca
0x1 <kevin.francart3@gmail.com>
2026-08-27 21:30:25 +0200

    misc

--- src/Node/FS/Stream.go
+++ src/Node/FS/Stream.go
@@ -2,18 +2,17 @@ package Node_FS_Stream
 
 import (
 	"os"
-	"gopurs/output/Node.EventEmitter"
 	"gopurs/output/gopurs_runtime"
 )
 
 func CreateWriteStreamImpl(filepath string) interface{} {
 	f, _ := os.OpenFile(filepath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
-	return Node_EventEmitter.NewImpl(f)
+	return Node_EventEmitter_NewImpl(f)
 }
 
 func CreateWriteStreamOptsImpl(filepath string, opts interface{}) interface{} {
 	f, _ := os.OpenFile(filepath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
-	return Node_EventEmitter.NewImpl(f)
+	return Node_EventEmitter_NewImpl(f)
 }
 
 func FdCreateWriteStreamImpl(fd interface{}) interface{} {
@@ -24,7 +23,7 @@ func FdCreateWriteStreamImpl(fd interface{}) interface{} {
 		fdFloat = v
 	}
 	f := os.NewFile(uintptr(fdFloat), "")
-	return Node_EventEmitter.NewImpl(f)
+	return Node_EventEmitter_NewImpl(f)
 }
 
 func FdCreateWriteStreamOptsImpl(fd interface{}, opts interface{}) interface{} {
@@ -35,17 +34,17 @@ func FdCreateWriteStreamOptsImpl(fd interface{}, opts interface{}) interface{} {
 		fdFloat = v
 	}
 	f := os.NewFile(uintptr(fdFloat), "")
-	return Node_EventEmitter.NewImpl(f)
+	return Node_EventEmitter_NewImpl(f)
 }
 
 func CreateReadStreamImpl(filepath string) interface{} {
 	f, _ := os.Open(filepath)
-	return Node_EventEmitter.NewImpl(f)
+	return Node_EventEmitter_NewImpl(f)
 }
 
 func CreateReadStreamOptsImpl(filepath string, opts interface{}) interface{} {
 	f, _ := os.Open(filepath)
-	return Node_EventEmitter.NewImpl(f)
+	return Node_EventEmitter_NewImpl(f)
 }
 
 func FdCreateReadStreamImpl(fd interface{}) interface{} {
@@ -56,7 +55,7 @@ func FdCreateReadStreamImpl(fd interface{}) interface{} {
 		fdFloat = v
 	}
 	f := os.NewFile(uintptr(fdFloat), "")
-	return Node_EventEmitter.NewImpl(f)
+	return Node_EventEmitter_NewImpl(f)
 }
 
 func FdCreateReadStreamOptsImpl(fd interface{}, opts interface{}) interface{} {
@@ -67,5 +66,5 @@ func FdCreateReadStreamOptsImpl(fd interface{}, opts interface{}) interface{} {
 		fdFloat = v
 	}
 	f := os.NewFile(uintptr(fdFloat), "")
-	return Node_EventEmitter.NewImpl(f)
+	return Node_EventEmitter_NewImpl(f)
 }
